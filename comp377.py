6a6496628665841a71cfe31b14965708b1dde677
Maatheralgharibi <146645945+Maatheralgharibi@users.noreply.github.com>
2023-12-02 13:17:07 +0000

    1

---
+++ comp377.py
@@ -0,0 +1,15 @@
+def ReadInt():
+    x = int (input("Enter an Integer: "))
+    return x
+
+def Max2(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+
+def add(x, y):
+    return (x + y)
+
+def mul(x, y):
+    return (x * y)
